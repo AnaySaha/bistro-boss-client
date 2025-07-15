42b4d56ab0fce36c0b250525d4454e81a908b7b7
Anay Saha <anaysahapaban@gmail.com>
2025-07-16 02:56:05 +0600

    section

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -3,17 +3,28 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
+
 import slide1 from '../../../assets/home/slide1.jpg';
 import slide2 from '../../../assets/home/slide2.jpg';
 import slide3 from '../../../assets/home/slide3.jpg';
 import slide4 from '../../../assets/home/slide4.jpg';
 import slide5 from '../../../assets/home/slide5.jpg';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
 
 
 const Category = () => {
     return (
-        <Swiper
-            slidesPerView={'auto'}
+   <section>
+        <SectionTitle
+        heading={"Order Online"}
+        subHeading={"From 11.00am to 10.00pm"}
+
+        >
+
+        </SectionTitle>
+         <Swiper
+            slidesPerView={4}
             centeredSlides={true}
             spaceBetween={30}
             pagination={{
@@ -40,6 +51,9 @@ const Category = () => {
             </SwiperSlide>
          
         </Swiper>
+   </section>
+
+
     );
 };
 
