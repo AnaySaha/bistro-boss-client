d8f7e393fcb2e5ae0e0a939320bb462c72b2fdca
Anay Saha <anaysahapaban@gmail.com>
2025-07-18 02:23:47 +0600

    add

--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -4,13 +4,13 @@ const MenuItem = ({item}) => {
     const { name, image, price, recipe} = item;
     return (
         <div className='flex space-x-2'>
-            <img style={{borderRadius: '0  200px 200px 200px '}} className=' w-[10px]' src={image} alt="" />
+            <img style={{borderRadius: '0 200px 200px 200px '}} className=' w-[120px]' src={image} alt="" />
           <div>
             <h3 className='uppercase'>
                 {name} ----------
             </h3>
             <p className='text-yellow-400'>{recipe}</p>
-   
+
           </div>
          <p>{price}</p>
         </div>
