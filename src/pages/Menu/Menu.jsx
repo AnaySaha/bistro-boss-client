1acbd5ca14f2734949ff8acbfeddaa8ad9bdf3f0
Anay Saha <anaysahapaban@gmail.com>
2025-07-29 03:33:49 +0600

    menu

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,13 +1,13 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../Shared/Cover/Cover';
+import menuImg from '../../assets/menu/dessert-bg.jpeg';
 
 
 const Menu = () => {
     return (
         <div>
-        <Helmet>
-        <title>BISTRO | MENU</title>
-        </Helmet>
-            <h2>This is Menu</h2>
+     <Helmet><title> BISTRO | MENU</title></Helmet>
+           <Cover img={menuImg}></Cover>
         </div>
     );
 };
