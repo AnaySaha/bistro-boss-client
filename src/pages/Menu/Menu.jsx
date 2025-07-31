e9c21a11897bca71d269532c3d8d6b33b3dffffe
Anay Saha <anaysahapaban@gmail.com>
2025-08-01 03:22:51 +0600

    paralax

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,13 +1,18 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 import menuImg from '../../assets/menu/dessert-bg.jpeg';
-
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
         <div>
      <Helmet><title> BISTRO | MENU</title></Helmet>
-           <Cover img={menuImg}></Cover>
+           <Cover img={menuImg} title="our menu"></Cover>
+           <PopularMenu></PopularMenu>
+           <Cover img={menuImg} title="our menu"></Cover>
+           <PopularMenu></PopularMenu>
+           <Cover img={menuImg} title="our menu"></Cover>
+           <PopularMenu></PopularMenu>
         </div>
     );
 };
