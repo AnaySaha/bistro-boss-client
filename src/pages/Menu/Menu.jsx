b014205cb1150c0641cbd54ca20c7f54c21b715e
Anay Saha <anaysahapaban@gmail.com>
2025-08-03 02:59:53 +0600

    hook

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -2,9 +2,24 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 import menuImg from '../../assets/menu/dessert-bg.jpeg';
 import PopularMenu from '../Home/PopularMenu/PopularMenu';
+import useMenu from '../../hooks/useMenu';
 
 const Menu = () => {
+
+        // const [menu, setMenu] = useState([]);
+    // useEffect ( () =>{
+    //     fetch('menu.json')
+    //     .then(res => res.json())
+    //     .then(
+    //         data => {
+    //             const popularItems = data.filter(item => item.category === 'popular');
+    //             setMenu(popularItems)
+    //         }
+    //     )
+    // }, [])
+    
     return (
+
         <div>
      <Helmet><title> BISTRO | MENU</title></Helmet>
            <Cover img={menuImg} title="our menu"></Cover>
@@ -13,6 +28,7 @@ const Menu = () => {
            <PopularMenu></PopularMenu>
            <Cover img={menuImg} title="our menu"></Cover>
            <PopularMenu></PopularMenu>
+         
         </div>
     );
 };
