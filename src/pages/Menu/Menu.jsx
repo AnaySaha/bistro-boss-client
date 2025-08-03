65db110beabccef767f9ee77044eafacf3f59c0c
Anay Saha <anaysahapaban@gmail.com>
2025-08-04 03:11:27 +0600

    joss

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -3,8 +3,19 @@ import Cover from '../Shared/Cover/Cover';
 import menuImg from '../../assets/menu/dessert-bg.jpeg';
 import PopularMenu from '../Home/PopularMenu/PopularMenu';
 import useMenu from '../../hooks/useMenu';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
 const Menu = () => {
+
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+ 
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+ 
+    const offered = menu.filter(item => item.category === 'offered');
+ 
 
         // const [menu, setMenu] = useState([]);
     // useEffect ( () =>{
@@ -24,6 +35,7 @@ const Menu = () => {
      <Helmet><title> BISTRO | MENU</title></Helmet>
            <Cover img={menuImg} title="our menu"></Cover>
            <PopularMenu></PopularMenu>
+           <SectionTitle subHeading= "Don't Miss" heading={"Today's Offer"}></SectionTitle>
            <Cover img={menuImg} title="our menu"></Cover>
            <PopularMenu></PopularMenu>
            <Cover img={menuImg} title="our menu"></Cover>
