14f02bc2435f2d9d28940c36062c14864f8145c8
Anay Saha <anaysahapaban@gmail.com>
2025-08-05 01:11:54 +0600

    pizza

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,14 +1,17 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 import menuImg from '../../assets/menu/dessert-bg.jpeg';
+import dessertImg from '../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../assets/menu/pizza-bg.jpg';
 import PopularMenu from '../Home/PopularMenu/PopularMenu';
 import useMenu from '../../hooks/useMenu';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import MenuCategory from './MenuCategory/MenuCategory';
 
 const Menu = () => {
 
     const [menu] = useMenu();
-    const dessert = menu.filter(item => item.category === 'dessert');
+    const desserts = menu.filter(item => item.category === 'dessert');
     const soup = menu.filter(item => item.category === 'soup');
  
     const salad = menu.filter(item => item.category === 'salad');
@@ -34,13 +37,35 @@ const Menu = () => {
         <div>
      <Helmet><title> BISTRO | MENU</title></Helmet>
            <Cover img={menuImg} title="our menu"></Cover>
-           <PopularMenu></PopularMenu>
+
+           {/* manin */}
+
+
+           {/* <PopularMenu></PopularMenu> */}
            <SectionTitle subHeading= "Don't Miss" heading={"Today's Offer"}></SectionTitle>
+           
+           <MenuCategory items={offered}></MenuCategory>
+           
+           
+           {/* <PopularMenu></PopularMenu>
            <Cover img={menuImg} title="our menu"></Cover>
-           <PopularMenu></PopularMenu>
-           <Cover img={menuImg} title="our menu"></Cover>
-           <PopularMenu></PopularMenu>
-         
+           <PopularMenu></PopularMenu> */}
+
+           {/* dessert items */}
+
+           <MenuCategory
+           items={desserts}
+            title={"Dessert"}
+            img={dessertImg}
+            ></MenuCategory>
+
+
+            <MenuCategory
+           items={pizza}
+            title={"Pizza"}
+            img={pizzaImg}
+            ></MenuCategory>
+        
         </div>
     );
 };
