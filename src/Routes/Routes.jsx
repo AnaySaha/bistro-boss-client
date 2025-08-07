5b7923672f4a4dd78bb7ca0133afcd1a780ae662
Anay Saha <anaysahapaban@gmail.com>
2025-08-07 11:54:31 +0600

    foodcard

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import {
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home";
 import Menu from "../pages/Menu/Menu";
+import Order from "../pages/Order/Order";
 
   
  export const router = createBrowserRouter([
@@ -21,6 +22,12 @@ import Menu from "../pages/Menu/Menu";
         {
             path: 'menu',
             element: <Menu></Menu>
+        },
+
+        {
+
+            path: 'order',
+            element: <Order></Order>
         }
     ]
     }
