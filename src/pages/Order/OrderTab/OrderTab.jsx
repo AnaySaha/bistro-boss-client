c46e0c14237c943bcf34e436d68191fc228d9f8d
Anay Saha <anaysahapaban@gmail.com>
2025-08-16 03:38:00 +0600

    swiper

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+
 import FoodCard from '../../../components/SectionTitle/FoodCard/FoodCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 const OrderTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      };
+    
+
     return (
+        <div >
+    
+
+
+<Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
         <div className='grid md:grid-cols-3 gap-10'>
         {
             items.map(item => <FoodCard
@@ -10,6 +32,10 @@ const OrderTab = ({ items }) => {
                 item={item}
             ></FoodCard>)
         }
+        </div>
+        </SwiperSlide>
+          
+      </Swiper>
         </div>
     );
 };
