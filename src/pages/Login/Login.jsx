54659c9bb413b582d0a020648f4ee4cccca8407f
Anay Saha <anaysahapaban@gmail.com>
2025-08-19 11:54:13 +0600

    captcha

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,10 @@
+import { useRef, useState } from 'react';
 import { useEffect } from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 
 const Login = () => {
-
+    const captchaRef = useRef(null);
+    const [disabled, setDisabled] = useState(true);
     useEffect( ()=> {
         loadCaptchaEnginge(6);
     }, [
@@ -15,6 +17,18 @@ const Login = () => {
         const password = form.password.value;
         console.log(email, password);
     }
+
+   const handleValidateCaptcha = e => {
+    const user_captcha_value = captchaRef.current.value;
+    if (validateCaptcha(user_captcha_value)){
+        setDisabled(false);
+    }
+    else{
+        setDisabled(true)
+    }
+
+   } 
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex md:flex lg:flex-row-reverse">
@@ -46,11 +60,11 @@ const Login = () => {
                             <label className="label">
                                    <LoadCanvasTemplate />
                             </label>
-                            <input type="text" name="captcha"  placeholder="type the text above" className="input input-bordered" required />
-                            
+                            <input type="text" ref={captchaRef} name="captcha"  placeholder="type the text above" className="input input-bordered" required />
+                            <button onClick={handleValidateCaptcha} className="btn btn-outline btn-xs mt-2">Validate</button>
                         </div>
                         <div className="form-control mt-6">
-                            <input type='submit' value="Login"
+                            <input disabled={disabled} type='submit' value="Login"
                             className="btn btn-primary"/>
                         </div>
                     </form>
