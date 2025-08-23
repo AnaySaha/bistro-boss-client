b6f080da04b12d3df09676733a16a44059efec10
Anay Saha <anaysahapaban@gmail.com>
2025-08-24 03:16:06 +0600

    password validation

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -33,14 +33,35 @@ const SignUp = () => {
                   <span className="label-text">Email</span>
                 </label>
                 <input type="email" {...register("email", { required: true }) } name="email" placeholder="email" className="input input-bordered"/>
-                {errors.name && <span className="text-red-600">Email is required</span>}
+                {errors.email && <span className="text-red-600">Email is required</span>}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" {...register("password", { required: true, minLength:6, maxLength:10 }) }  name="password" placeholder="password" className="input input-bordered"/>
-                {errors.name && <span className="text-red-600">Password is required</span>}
+                <input type="password" {...register("password", { required: true, 
+                  minLength:8, 
+                  maxLength:12,
+                  pattern: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])$/
+                  }) }  name="password" placeholder="password" className="input input-bordered"/>
+                {errors.password?.type === 'required' && <p className="
+                text-red-600">
+                  Password is Requied
+                </p>}
+                {errors.password?.type === 'minLength' && <p className="
+                text-red-600">
+                  Password must be 8 characters
+                </p>}
+                {errors.password?.type === 'maxLength' && <p className="
+                text-red-600">
+                  Password must be less then 12 characters
+                </p>}
+                {errors.password?.type === 'pattern' && <p className="
+                text-red-600">
+                  Password must be have one upper case, one lower case, one number
+                  and one special characters
+                </p>}
+
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
