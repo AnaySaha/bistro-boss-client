c9e3ed04efab57c21c94f131d2e15e4982afb1b3
Anay Saha <anaysahapaban@gmail.com>
2025-08-24 03:57:39 +0600

    create user

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,19 +1,31 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { data, Link } from "react-router-dom";
+import { AuthContext } from "../../firebase/providers/AuthProviders";
 
 
 
 
 
 const SignUp = () => {
-    const { handleSubmit, register, formState: { errors } } = useForm();
-    
-    const onSubmit = values => {
-        console.log(values);
-    }
-        
+  const { handleSubmit, register, formState: { errors } } = useForm();
 
-    return (
-        <div className="hero min-h-screen bg-base-200">
+  const { createUser } = useContext(AuthContext);
+  const onSubmit = data => {
+    console.log(data);
+    createUser(data.email, data.password)
+    .then(result => {
+      const loggeduser = result.user;
+      console.log(loggeduser);
+    })
+  }
+
+
+  return (
+    <>
+     <Helmet><title> BISTRO | SIGN UP</title></Helmet>
+      <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">SignUp now!</h1>
@@ -25,25 +37,26 @@ const SignUp = () => {
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input type="text" {...register("name", { required: true }) } name='name' placeholder="name" className="input input-bordered"/>
+                <input type="text" {...register("name", { required: true })} name='name' placeholder="name" className="input input-bordered" />
                 {errors.name && <span className="text-red-600">Name is required</span>}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" {...register("email", { required: true }) } name="email" placeholder="email" className="input input-bordered"/>
+                <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
                 {errors.email && <span className="text-red-600">Email is required</span>}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" {...register("password", { required: true, 
-                  minLength:8, 
-                  maxLength:12,
-                  pattern: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])$/
-                  }) }  name="password" placeholder="password" className="input input-bordered"/>
+                <input type="password" {...register("password", {
+                  required: true,
+                  minLength: 8,
+                  maxLength: 12,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                })} name="password" placeholder="password" className="input input-bordered" />
                 {errors.password?.type === 'required' && <p className="
                 text-red-600">
                   Password is Requied
@@ -67,13 +80,17 @@ const SignUp = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <input type='submit' value="Sign Up"
+                  className="btn btn-primary" />
               </div>
             </form>
+            <p><small>Alread have an Account? <Link to="/Login"> Login
+                        </Link> </small></p>
           </div>
         </div>
       </div>
-    );
+    </>
+  );
 };
 
 export default SignUp;
